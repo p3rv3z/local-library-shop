42b621824b14b4375d17b82370796ec192d94353
Pervez Ali <contact.pervezali@gmail.com>
2021-10-20 21:13:55 +0600

    fix: paginator ui issue solved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,34 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->paginatorConfiguration();
+  }
+
+  private function paginatorConfiguration()
+  {
+    Paginator::useBootstrap();
+    Paginator::defaultView('components.pagination.default');
+  }
 }
